e97c3b8fd5c7bf227457030c8677308b0a7fda1e
abdusamad8575 <samadpts786313@gmail.com>
2024-10-01 13:15:58 +0530

    client updation

--- tharavaduclient/src/App.jsx
+++ tharavaduclient/src/App.jsx
@@ -7,20 +7,23 @@ import Contact from './pages/Contact'
 import Login from './pages/Login'
 import Register from './pages/Register'
 import Events from './pages/Events'
+import ScrollToTop from "./ScrollToTop";
 
 function App() {
 
   return (
     <>
-    <Routes>
-      <Route path='/' element={<Home/>}  />
-      <Route path='/about' element={<About/>}  />
-      <Route path='/gallery' element={<Gallery/>}  />
-      <Route path='/contact' element={<Contact/>}  />
-      <Route path='/login' element={<Login/>}  />
-      <Route path='/register' element={<Register/>}  />
-      <Route path='/events' element={<Events/>}  />
-    </Routes>
+      <ScrollToTop />
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/about' element={<About />} />
+        <Route path='/gallery' element={<Gallery />} />
+        <Route path='/contact' element={<Contact />} />
+        <Route path='/login' element={<Login />} />
+        <Route path='/register' element={<Register />} />
+        <Route path='/events' element={<Events />} />
+      </Routes>
+      <ScrollToTop />
     </>
   )
 }
